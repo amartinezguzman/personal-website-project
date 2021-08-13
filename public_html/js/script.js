a9873c1cee6005b6d9aea9146ebebb6e42342314
Adriana Martinez <amartinezguzma@cnm.edu>
2021-08-13 08:55:28 -0600

    Successfully added contact form.

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,51 +1,52 @@
-$(document).ready(function(){
+$(document).ready(function () {
     document.getElementById('contact')
     $('#contact').validate({
         debug: true,
         errorClass: 'alert alert-danger',
         ErrorLabelContainer: '#output-area',
         errorElement: 'div',
-       //These are the rules to define what data we want and don't want
+        //These are the rules to define what data we want and don't want
         // Each of these groups start with the form input name attribute
         rules: {
-            name:{
+            name: {
                 required: true
             },
-            email:{
+            email: {
                 email: true,
                 required: true
             },
-            message:{
+            message: {
                 required: true,
                 maxlength: 2000
             }
         },
         //Add error responses
         messages: {
-            name:{
+            name: {
                 required: 'Name is required'
             },
             email: {
                 email: 'Please provide valid email.',
                 required: 'Email is required'
             },
-            message:{
+            message: {
                 required: 'Message is required.',
                 maxlength: 'Message must be 2000 characters or less.'
             }
         },
         //Define what happens when someone submits forms
-        submitHandler:() => {
-            $('#contact').ajaxSubmit({
-                type:'POST',
-                url: $('#contact').attr('action'),
-                success:(ajaxOutput) => {
-                    $("output-area").css('display', '')
-                    $('output-area').html(ajaxOutput)
 
-                if($('.alert-success'>= 1)){
-                    $('#contact')[0].reset()
-                }
+        submitHandler: (form) => {
+            $('#contact').ajaxSubmit({
+                type: 'POST',
+                url: $('#contact').attr('action'),
+                success: (ajaxOutput) => {
+                    $("#output-area").css('display', '')
+                    $('#output-area').html(ajaxOutput)
+
+                    if ($('.alert-success' >= 1)) {
+                        $('#contact')[0].reset()
+                    }
                 }
             })
         }
